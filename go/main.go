f5910dfa71f49d1b40fa1f777ce68e05fa3865ad
Julius Volz <julius@soundcloud.com>
2017-07-01 16:20:26 +0200

    Add example instrumentation

--- go/main.go
+++ go/main.go
@@ -6,13 +6,35 @@ import (
 	"math/rand"
 	"net/http"
 	"time"
+
+	"github.com/prometheus/client_golang/prometheus"
 )
 
-type demoAPI struct{}
+type demoAPI struct {
+	requestDurations *prometheus.HistogramVec
+}
+
+func newDemoAPI(reg prometheus.Registerer) *demoAPI {
+	return &demoAPI{
+		requestDurations: prometheus.NewHistogramVec(prometheus.HistogramOpts{
+			Name:    "some_api_http_request_duration_seconds",
+			Help:    "A histogram of the demo API request durations in seconds.",
+			Buckets: prometheus.LinearBuckets(.05, .025, 10),
+		}, []string{"handler"}),
+	}
+}
 
 func (a demoAPI) register(mux *http.ServeMux) {
-	mux.HandleFunc("/api/foo", a.foo)
-	mux.HandleFunc("/api/bar", a.bar)
+	instr := func(handler string, fn http.HandlerFunc) http.HandlerFunc {
+		return func(w http.ResponseWriter, r *http.Request) {
+			start := time.Now()
+			fn(w, r)
+			a.requestDurations.WithLabelValues(handler).Observe(float64(time.Since(start)))
+		}
+	}
+
+	mux.HandleFunc("/api/foo", instr("foo", a.foo))
+	mux.HandleFunc("/api/bar", instr("bar", a.bar))
 }
 
 func (a demoAPI) foo(w http.ResponseWriter, r *http.Request) {
@@ -32,7 +54,16 @@ func (a demoAPI) bar(w http.ResponseWriter, r *http.Request) {
 	w.Write([]byte("Handled bar"))
 }
 
-func backgroundTask() {
+func backgroundTask(reg prometheus.Registerer) {
+	totalCount := prometheus.NewCounter(prometheus.CounterOpts{
+		Name: "background_task_runs_total",
+		Help: "The total number of background task runs.",
+	})
+	failureCount := prometheus.NewCounter(prometheus.CounterOpts{
+		Name: "background_task_failures_total",
+		Help: "The total number of background task failures.",
+	})
+
 	log.Println("Starting background task loop...")
 	bgTicker := time.NewTicker(5 * time.Second)
 	for {
@@ -44,8 +75,10 @@ func backgroundTask() {
 		if rand.Float64() > 0.3 {
 			log.Println("Background task completed successfully.")
 		} else {
+			failureCount.Inc()
 			log.Println("Background task failed.")
 		}
+		totalCount.Inc()
 
 		<-bgTicker.C
 	}
@@ -54,9 +87,9 @@ func backgroundTask() {
 func main() {
 	listenAddr := flag.String("web.listen-addr", ":12345", "The address to listen on for web requests.")
 
-	go backgroundTask()
+	go backgroundTask(prometheus.DefaultRegisterer)
 
-	api := &demoAPI{}
+	api := newDemoAPI(prometheus.DefaultRegisterer)
 	api.register(http.DefaultServeMux)
 
 	log.Fatal(http.ListenAndServe(*listenAddr, nil))
--- python/server.py
+++ python/server.py
@@ -7,6 +7,7 @@ import time
 from BaseHTTPServer import BaseHTTPRequestHandler
 from BaseHTTPServer import HTTPServer
 from SocketServer import ThreadingMixIn
+from prometheus_client import Histogram, Counter, MetricsHandler, generate_latest, REGISTRY, CONTENT_TYPE_LATEST
 
 def handler_404(self):
     self.send_response(404)
@@ -26,14 +27,37 @@ def handler_bar(self):
     self.end_headers()
     self.wfile.write("Handled bar")
 
+def handler_metrics(self):
+    try:
+        output = generate_latest(REGISTRY)
+    except:
+        self.send_error(500, 'error generating metrics output')
+        raise
+    self.send_response(200)
+    self.send_header('Content-Type', CONTENT_TYPE_LATEST)
+    self.end_headers()
+    self.wfile.write(output)
+
 ROUTES = {
     "/api/foo": handler_foo,
     "/api/bar": handler_bar,
+    "/metrics": handler_metrics,
 }
 
 class Handler(BaseHTTPRequestHandler):
+    request_durations = Histogram(
+        "some_api_http_request_duration_seconds",
+        "A histogram of the demo API request durations in seconds.",
+        ["path"],
+        buckets=(.05, .075, .1, .125, .15, .175, .2, .225, .250, .275)
+    )
+
     def do_GET(self):
+        start = time.time()
+
         ROUTES.get(self.path, handler_404)(self)
+
+        self.request_durations.labels(path=self.path).observe(time.time() - start)
 
 class MultiThreadedHTTPServer(ThreadingMixIn, HTTPServer):
     pass
@@ -44,6 +68,15 @@ class Server(threading.Thread):
         httpd.serve_forever()
 
 def background_task():
+    total_count = Counter(
+        "background_task_runs_total",
+        "The total number of background task runs.",
+    )
+    failure_count = Counter(
+        "background_task_failures_total",
+        "The total number of background task failures.",
+    )
+
     logging.info("Starting background task loop...")
     while True:
         logging.info("Performing background task...")
@@ -54,7 +87,9 @@ def background_task():
         if random.random() > 0.3:
             logging.info("Background task completed successfully.")
         else:
+            failure_count.inc()
             logging.warn("Background task failed.")
+        total_count.inc()
 
         time.sleep(5)
 
